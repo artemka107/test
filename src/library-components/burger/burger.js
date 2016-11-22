4db16dd4387c04d81c730883e9b6f257f3210dfd
artemka107 <artem-ivanec@mail.ru>
2016-11-22 05:27:49 +0300

    Update 2016-11-22T02:27:44.190Z

--- src/library-components/burger/burger.js
+++
@@ -1,9 +0,0 @@
-var burger = document.querySelector('.burger');
-
-burger.addEventListener('click', function() {
-  if (burger.classList.contains('burger--closed')) {
-    burger.classList.remove('burger--closed');
-  } else {
-    burger.classList.add('burger--closed');
-  }
-});
